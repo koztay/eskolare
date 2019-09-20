6ade66e67a1176034cf456828238e1b461ad5d3a
koztay <koztay@me.com>
2019-09-21 00:20:09 +0300

    deleted unnecessary folders and files

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -82,9 +82,6 @@ LOCAL_APPS = [
     # Your stuff: custom apps go here
     "books",
     "categories",
-    # "posts",
-    # "products",
-    # "sellers",
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
--- backend/sellers/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-from .models import Seller, Address
-
-admin.site.register(Seller)
-admin.site.register(Address)
